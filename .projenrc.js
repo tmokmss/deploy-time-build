9d9b335af14d9fadb3a508c23cae54e6b4fa34fc
tmokmss <tomookam@live.jp>
2022-05-12 23:55:23 +0900

    fix repository metadata

--- .projenrc.js
+++ .projenrc.js
@@ -1,12 +1,14 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Masashi Tomooka',
-  authorAddress: 'mtomooka@amazon.co.jp',
-  cdkVersion: '2.1.0',
+  author: 'tmokmss',
+  authorAddress: 'tomookam@live.jp',
+  cdkVersion: '2.20.0',
   defaultReleaseBranch: 'main',
   name: 'deploy-time-build',
-  repositoryUrl: 'https://github.com/mtomooka/deploy-time-build.git',
-
+  repositoryUrl: 'https://github.com/tmokmss/deploy-time-build.git',
+  eslintOptions: {
+    ignorePatterns: ['example/**/*'],
+  },
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
