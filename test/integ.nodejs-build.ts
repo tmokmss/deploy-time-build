4867ee891904027df1f40cf27f09c786f095e21a
tmokmss <tomookam@live.jp>
2023-01-17 13:50:13 +0900

    chore: add an integration test powered by integ-test and integ-runner

---
+++ test/integ.nodejs-build.ts
@@ -0,0 +1,44 @@
+import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+import { Stack, StackProps, App, RemovalPolicy } from 'aws-cdk-lib';
+import { BlockPublicAccess, Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+import { NodejsBuild } from '../src/nodejs-build';
+
+const app = new App();
+
+class TestStack extends Stack {
+  constructor(scope: Construct, id: string, props: StackProps = {}) {
+    super(scope, id, props);
+
+    const dstBucket = new Bucket(this, 'Destination', {
+      autoDeleteObjects: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      encryption: BucketEncryption.S3_MANAGED,
+    });
+    const dstPath = '/';
+
+    new NodejsBuild(this, 'ExampleBuild', {
+      assets: [
+        {
+          path: '../example/example-app',
+          exclude: ['dist', 'node_modules'],
+        },
+      ],
+      destinationBucket: dstBucket,
+      destinationKeyPrefix: dstPath,
+      outputSourceDirectory: 'dist',
+      buildCommands: ['npm ci', 'npm run build'],
+      buildEnvironment: {
+        VITE_SOME_TOKEN: dstBucket.bucketName,
+      },
+    });
+  }
+}
+
+const stack = new TestStack(app, 'NodejsBuildIntegTest');
+
+new IntegTest(app, 'Test', {
+  testCases: [stack],
+  diffAssets: true,
+});
